8d69f7f329acba49641355959bd3d6ad7ad08a31
amani-joseph <mr.amanijoseph@gmail.com>
2022-03-24 10:28:09 +0300

    Added some dummy photos url. Gallery is working okay

--- gallerie/gallery/views.py
+++ gallerie/gallery/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# DUMMY DATA
+photos = ['https://images.pexels.com/photos/5011647/pexels-photo-5011647.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','https://images.pexels.com/photos/1421903/pexels-photo-1421903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1','https://images.pexels.com/photos/1612353/pexels-photo-1612353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',]
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>GALLERY WORKS</h1>')
+    context = {
+        'photos': photos
+    }
+    return render(request, 'gallery/home.html', context)
